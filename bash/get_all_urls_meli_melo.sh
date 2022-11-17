e34eed7ec1a3207ff5e549e2f87353e2bb852de1
刘英姿 <liuyingzi37@gmail.com>
2022-11-17 01:26:04 +0100

    url_francais et bash

---
+++ bash/get_all_urls_meli_melo.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#1st argument = word
+#2nd and more arguments = path/to/file/containing/urls.txt
+
+# If there are less than two arguments
+if [[ $# -lt 2 ]]
+then
+	echo "Minimum two argument."
+	echo "Use :"
+	echo "    ./get_all_urls.sh [WORD] [FILES...]"
+	exit 1
+fi
+
+# WORD will be the name of the directory to store the generated data
+WORD="$1"
+
+# shift removes the first argument
+shift
+
+# For every arguments after WORD
+for FILE in $@
+do
+	if [ ! -f $FILE ]
+	then
+		echo "$FILE does not exist."
+		echo "It should be a file containing a list of urls."
+	else
+		./get_url_meli_melo.sh "$WORD" $FILE
+	fi
+done
