9de1771d456498d344a2266d32398a031d6b2a1a
Sazuna <liza.fretel@gmail.com>
2022-11-23 15:47:04 +0100

    Prend en compte le code de retour pour HTTP/1.1

--- bash/get_response_code.sh
+++ bash/get_response_code.sh
@@ -3,4 +3,4 @@
 RESPONSE=$1
 
 # commande qui recupère le code HTTP de la réponse
-echo $RESPONSE | egrep -o "^HTTP/.? [0-9]+" | tail -n 1 | cut -d' ' -f2
+echo $RESPONSE | egrep -o "^HTTP/.* [0-9]+" | tail -n 1 | cut -d' ' -f2
