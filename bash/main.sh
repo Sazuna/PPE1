fd51903bd157e27386d5805218a25894c3907044
kittog <lena.gaubert@gmail.com>
2022-11-30 11:26:09 +0100

    Nouveau script : get_all_urls pour toutes nos langues + mots

---
+++ bash/main.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# get all urls 
+# for each word in each language
+
+# FRENCH
+urlsListFR=$(ls urls/presse/francais/sens_*)
+./get_all_urls.sh "méli-mélo" $urlsListFR
+
+# SPANISH
+urlsListSP=$(ls urls/presse/espagnol/sens_*)
+./get_all_urls.sh "batiburrillo" $urlsListSP
+
+# CHINESE
+urlsListCN=$(ls urls/presse/chinois/sens_*)
+./get_all_urls.sh "dazahui" $urlsListCN
