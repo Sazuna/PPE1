ea52a3fcd800de66eb87c0faf1ac7c6c790ad900
Sazuna <liza.fretel@gmail.com>
2022-11-30 14:38:43 +0100

    Création des dossiers inexistants

---
+++ bash/mkdirs.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# If the directory does not exist yet
+if [ ! -d "../codes" ]
+then
+	mkdir "../codes"
+fi
+
+if [ ! -d "../csv" ]
+then
+	mkdir "../csv"
+fi
+
+if [ ! -d "../aspirations" ]
+then
+	mkdir "../aspirations"
+fi
+
+if [ ! -d "../contexts" ]
+then
+	mkdir "../contexts"
+fi
+
+if [ ! -d "../dump-html" ]
+then
+	mkdir "../dump-html"
+fi
+
+if [ ! -d "../dump-texts" ]
+then
+	mkdir "../dump-texts"
+fi
+
+if [ ! -d "../occurrences" ]
+then
+	mkdir "../occurrences"
+fi
+
+if [ ! -d "../html" ]
+then
+	mkdir "../html"
+fi
