13a2ed45a19aae3a66f8922d7d6a15bb61ba11a5
kittog <lena.gaubert@gmail.com>
2022-12-14 11:09:49 +0100

    Nouveau script : toutes les tables ensemble dans un seul fichier html

---
+++ bash/all_tables.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+PATH='../html/tables.html'
+
+echo '<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Hello Bulma!</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
+  </head>
+  <body>' > $PATH
+
+cat "../html/batiburrillo-table.html" >> $PATH
+cat ../html/dazahui-table.html >> $PATH
+cat ../html/méli-mélo-table.html >> $PATH
+
+echo '</body>
+</html>' >> $PATH
