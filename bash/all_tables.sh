2d8ea44a02d1b003c8eb5c1023bb98cc4d921691
Sazuna <liza.fretel@gmail.com>
2023-01-03 16:10:59 +0100

    Mise à jour de make_page.sh et all_tables.sh

---
+++ bash/all_tables.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+TABLE='../html/tables.html'
+STRUCT='../html/struct.html'
+
+cat $STRUCT | sed -n '/{CONTENT}/q;p' > $TABLE
+
+echo "<h3>Espagnol - batiburrillo</h3>" >> $TABLE
+echo "<div class='box'>" >> $TABLE
+cat "../html/content/batiburrillo-table.html" >> $TABLE
+echo "</div>" >> $TABLE
+
+echo "<h3>Chinois - 大杂烩</h3>" >> $TABLE
+echo "<div class='box'>" >> $TABLE
+cat "../html/content/dazahui-table.html" >> $TABLE
+echo "</div>" >> $TABLE
+
+echo "<h3>Français - Méli-mélo</h3>" >> $TABLE
+echo "<div class='box'>" >> $TABLE
+cat "../html/content/méli-mélo-table.html" >> $TABLE
+echo "</div>" >> $TABLE
+
+tac $STRUCT | sed -n '/{CONTENT}/q;p' | tac >> $TABLE
--- bash/make_page.sh
+++ bash/make_page.sh
@@ -5,7 +5,7 @@ OUTPUT_HTML=$2 # The result page
 STRUCT="../html/struct.html"
 
 # On récupère l'en-tête de la page, la barre de navigation etc
-cat $STRUCT | sed -n '/{CONTENT/q;p' >> $OUTPUT_HTML
+cat $STRUCT | sed -n '/{CONTENT}/q;p' > $OUTPUT_HTML
 
 # On récupère le contenu à injecter dans la page
 cat $INPUT_HTML >> $OUTPUT_HTML
