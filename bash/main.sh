1fdbe3318a4e74adcf3e9528c21ffc01b7a61361
kittog <lena.gaubert@gmail.com>
2023-01-03 21:44:37 +0100

    Tableau pour l'espagnol - batiburrillo

--- bash/main.sh
+++ bash/main.sh
@@ -7,13 +7,13 @@
 #urlsListFR=$(ls ../url/presse/francais/test*)
 #./get_all_urls.sh "méli-mélo" $urlsListFR
 
-./all_tables.sh
+#./all_tables.sh
 
 # # SPANISH
-# urlsListSP=$(ls ../url/presse/espagnol/batiburrillo/sens_*)
-# ./get_all_urls.sh "batiburrillo" $urlsListSP
+urlsListSP=$(ls ../url/presse/espagnol/batiburrillo/sens_propre.txt)
+./get_all_urls.sh "batiburrillo" $urlsListSP
 
 # # CHINESE
-urlsListCN=$(ls ../url/presse/chinois/dazahui/sens_*)
-./get_all_urls.sh -n "dazahui" $urlsListCN
+#urlsListCN=$(ls ../url/presse/chinois/dazahui/sens_*)
+#./get_all_urls.sh -n "dazahui" $urlsListCN
 # # -n because we want to remove spaces and \n in the context.
