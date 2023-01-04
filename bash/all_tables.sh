0bcdbb434101cd30d6aad34cd943997de99aba58
Sazuna <liza.fretel@gmail.com>
2023-01-04 10:58:07 +0100

    all_tables.sh

--- bash/all_tables.sh
+++ bash/all_tables.sh
@@ -5,19 +5,19 @@ STRUCT='../html/struct.html'
 
 cat $STRUCT | sed -n '/{CONTENT}/q;p' > $TABLE
 
-# echo "<h3>Espagnol - batiburrillo</h3>" >> $TABLE
-# echo "<div class='box'>" >> $TABLE
-# cat "../html/content/batiburrillo-table.html" >> $TABLE
-# echo "</div>" >> $TABLE
+echo "<h3>Espagnol - batiburrillo</h3>" >> $TABLE
+echo "<div class='box'>" >> $TABLE
+cat "../html/content/batiburrillo-table.html" >> $TABLE
+echo "</div>" >> $TABLE
 
-# echo "<h3>Chinois - 大杂烩</h3>" >> $TABLE
-# echo "<div class='box'>" >> $TABLE
-# cat "../html/content/dazahui-table.html" >> $TABLE
-# echo "</div>" >> $TABLE
+echo "<h3>Chinois - 大杂烩</h3>" >> $TABLE
+echo "<div class='box'>" >> $TABLE
+cat "../html/content/dazahui-table.html" >> $TABLE
+echo "</div>" >> $TABLE
 
 echo "<h3>Français - Méli-mélo</h3>" >> $TABLE
 echo "<div class='box'>" >> $TABLE
 cat "../html/content/méli-mélo-table.html" >> $TABLE
 echo "</div>" >> $TABLE
 
-tail -r $STRUCT | sed -n '/{CONTENT}/q;p' | tail -r >> $TABLE
+tac $STRUCT | sed -n '/{CONTENT}/q;p' | tac >> $TABLE
