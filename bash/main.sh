f6bd922d120beb241615123bdd483000fe40e8b5
刘英姿 <liuyingzi37@gmail.com>
2023-01-09 13:29:33 +0100

    ajout des urls pour corpus meli-melo

--- bash/main.sh
+++ bash/main.sh
@@ -4,12 +4,12 @@
 # for each word in each language
 
 # FRENCH
-#urlsListFR=$(ls ../url/presse/francais/sens*)
-#./get_all_urls.sh "méli-mélo" $urlsListFR
+urlsListFR=$(ls ../url/presse/francais/sens*)
+./get_all_urls.sh "méli-mélo" $urlsListFR
 
-# # SPANISH
-#urlsListSP=$(ls ../url/presse/espagnol/batiburrillo/sens_propre.txt)
-#./get_all_urls.sh "batiburrillo" $urlsListSP
+# SPANISH
+urlsListSP=$(ls ../url/presse/espagnol/batiburrillo/sens_propre.txt)
+./get_all_urls.sh "batiburrillo" $urlsListSP
 
 # # CHINESE
  urlsListCN=$(ls ../url/presse/chinois/dazahui/sens_*)
