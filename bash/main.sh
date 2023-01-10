e76328fea33e7ea62e1ec5b6708d2fce17412fd7
刘英姿 <liuyingzi37@gmail.com>
2023-01-10 11:59:44 +0100

    Nettoyage

--- bash/main.sh
+++ bash/main.sh
@@ -7,11 +7,11 @@ FRENCH
 urlsListFR=$(ls ../url/presse/francais/sens*)
 ./get_all_urls.sh "méli-mélo" $urlsListFR
 
-# # SPANISH
-# urlsListSP=$(ls ../url/presse/espagnol/batiburrillo/sens_propre.txt)
-# ./get_all_urls.sh "batiburrillo" $urlsListSP
+# SPANISH
+urlsListSP=$(ls ../url/presse/espagnol/batiburrillo/sens_propre.txt)
+./get_all_urls.sh "batiburrillo" $urlsListSP
 
-# # # CHINESE
-# # urlsListCN=$(ls ../url/presse/chinois/dazahui/sens_*)
-# # ./get_all_urls.sh -n "dazahui" $urlsListCN
-# # -n because we want to remove spaces and \n in the context.
+# CHINESE
+urlsListCN=$(ls ../url/presse/chinois/dazahui/sens_*)
+./get_all_urls.sh -n "dazahui" $urlsListCN
+-n because we want to remove spaces and \n in the context.
